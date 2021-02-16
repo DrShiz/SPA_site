acf6e1ad30dd0c92afc766c6ed3457c27e8d30c1
DrShiz <68502612+DrShiz@users.noreply.github.com>
2021-02-16 12:58:13 +0300

    Update settings.py

--- SPA_blog/SPA_blog/settings.py
+++ SPA_blog/SPA_blog/settings.py
@@ -162,5 +162,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shizdryeah@gmail.com'
-EMAIL_HOST_PASSWORD = 'fbalxdpbvqkoqkcq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
